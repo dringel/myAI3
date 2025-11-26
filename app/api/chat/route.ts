c1a23d7fb962f3d9a5835aa6a7c0523de2a942de
7-a11y <adityapaliwal7@gmail.com>
2025-11-26 23:20:14 +0530

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
         system: SYSTEM_PROMPT,
         messages: convertToModelMessages(messages),
         tools: {
-            webSearch,
+            
             vectorDatabaseSearch,
         },
         stopWhen: stepCountIs(10),
